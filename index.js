a44e3376312e0d6503c0a404e9c9b62cac536530
RussellAbraham <russell_a@live.com>
2023-09-01 18:37:19 -0700

    Modify the driver code to pass the correct information to the constructors

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ class Withdrawal {
   constructor(amount, account) {
     this.amount = amount;
     this.account = account;
+    // if we are always going to commit after instancing this may help
+    //this.initialize.apply(this, arguments);
+  }
+  initialize(){
+    this.commit();
   }
   commit() {
     this.account.balance -= this.amount;
@@ -32,19 +37,20 @@ class Account {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
-
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
+console.log('Balance:', balance);
 
-const myAccount = new Account("snow-patrol");
+
